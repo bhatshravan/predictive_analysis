c75acb6e6010044cd62915c6d163dbdadec2f8de
shravan <shrvnbhat0@gmail.com>
2018-09-24 21:01:37 +0530

    firs

---
+++ p2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from datetime import datetime
+import csv
+import matplotlib.pyplot as plt 
+import matplotlib.dates as mdates
+from matplotlib import style
+
+headers = ['stocks','timestamp','open']
+
+df = pd.read_csv('ns.csv',index_col=1)
+# df.setindex('timestamp', inplace=True)
+print(df.head())
+
+style.use('ggplot')
+
+x = df['timestamp']
+y = df['open']
+
+#plt.plot(df['close'])
+#plt.plot(x,y)
+#plt.show()
